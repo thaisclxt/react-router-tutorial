11ce3116b96a2a3f2cee1f508c5885dfbf23623c
thaisclxt <thais.calixto@mail.uft.edu.br>
2022-04-18 16:57:53 -0300

    Read URL Params

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 import Expenses from "./routes/expenses";
 import Invoices from "./routes/invoices";
+import Invoice from "./routes/invoice";
 
 const rootElement = document.getElementById("root");
 render(
@@ -11,7 +12,9 @@ render(
 		<Routes>
 			<Route path="/" element={<App />}>
 				<Route path="expenses" element={<Expenses />} />
-				<Route path="invoices" element={<Invoices />} />
+				<Route path="invoices" element={<Invoices />}>
+					<Route path=":invoiceId" element={<Invoice />} />
+				</Route>
 				<Route
 					path="*" // It will match only when no other routes do
 					element={
