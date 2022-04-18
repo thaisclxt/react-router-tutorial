5e84b9b3999521cae0916dfc65a68b8cb3a9cc43
thaisclxt <thais.calixto@mail.uft.edu.br>
2022-04-18 16:46:51 -0300

    List the Invoices

--- src/routes/invoices.jsx
+++ src/routes/invoices.jsx
@@ -1,7 +1,26 @@
-export default function Expenses() {
-    return (
-      <main style={{ padding: "1rem 0" }}>
-        <h2>Expenses</h2>
-      </main>
-    );
-  }
+import { Link } from "react-router-dom";
+import { getInvoices } from "../data";
+
+export default function Invoices() {
+	let invoices = getInvoices();
+	return (
+		<div style={{ display: "flex" }}>
+			<nav
+				style={{
+					borderRight: "solid 1px",
+					padding: "1rem",
+				}}
+			>
+				{invoices.map((invoice) => (
+					<Link
+						style={{ display: "block", margin: "1rem 0" }}
+						to={`/invoices/${invoice.number}`}
+						key={invoice.number}
+					>
+						{invoice.name}
+					</Link>
+				))}
+			</nav>
+		</div>
+	);
+}
